9d3f39b2493219a84221f853815594447ebd4dc4
nancybordia <nancybordia@users.noreply.github.com>
2014-10-23 00:40:28 +0530

    Revised

--- src/ab/vision/Algebra.java
+++ src/ab/vision/Algebra.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 /**
- * Created by Nancy on 9/27/2014.
+ * Created by Nancy on 9/27/2014
  */
 public class Algebra {
     public boolean isB(ABObject x,ABObject y){
